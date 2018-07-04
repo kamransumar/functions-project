11340f6a42abfdcfd712fc138861c4643a122fa7
kamransumar <kamransumar@gmail.com>
2018-07-04 12:00:20 +0300

    added the javascript

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var product = function(number1, number2, number3) {
+  return number1 * number2 * number3
+}
+var number1=parseInt(prompt("enter a new number"))
+  var number2=parseInt(prompt("enter a another number"))
+  var number3=parseInt(prompt("enter a another number"))
+alert(product(number1,number2,number3))
